e347d272978e1cc7465141e8913dd44012eab76f
Sipang K <plaaom39@gmail.com>
2023-07-01 00:18:43 +0700

    chore(App.tsx): edit styles and add credit

--- src/App.tsx
+++ src/App.tsx
@@ -23,30 +23,36 @@ function App() {
   }, []);
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-darkBlue">
-      <div className="flex flex-col items-center bg-darkGrayishBlue w-[350px] h-[375px] md:w-[450px] xl:w-[500px] xl:h-[375px] rounded-[20px] px-10 py-10 relative">
-        <div className="mb-9">
-          <p className="text-neonGreen text-[11px] uppercase tracking-[0.4em]">
+    <main className="flex flex-col justify-center items-center w-screen h-screen bg-darkBlue">
+      <div className="flex flex-col items-center bg-darkGrayishBlue w-[350px] xl:w-[500px] p-[15rem] rounded-[20px] px-10 py-10 relative">
+        <div className="text-center">
+          <p className="text-neonGreen text-[11px] uppercase tracking-[0.4em] pb-8">
             Advice #{advice?.id}
           </p>
-        </div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-lightCyan text-[24px] text-center">"{advice?.advice}"</h1>
-        </div>
-        <div className="absolute top-[280px] px-10">
-          <img src="../static/images/pattern-divider-desktop.svg" alt="" />
-        </div>
-        <div className="absolute top-[340px]">
-          <button
-            type="button"
-            onClick={getQoute}
-            className="btn w-[60px] h-[60px] rounded-full bg-neonGreen flex justify-center items-center"
-          >
-            <img src="../static/images/icon-dice.svg" alt="" />
-          </button>
+          <h1 className="text-lightCyan text-[28px] pb-8">"{advice?.advice}"</h1>
+          <img
+            src="../static/images/pattern-divider-desktop.svg"
+            alt="pattern-divider-desktop"
+            className="pb-8"
+          />
         </div>
       </div>
-    </div>
+      <div>
+        <button
+          type="button"
+          onClick={getQoute}
+          className="btn p-4 mt-[-30px] fixed ml-[-28px] rounded-full bg-neonGreen"
+        >
+          <img src="../static/images/icon-dice.svg" alt="icon-dice" />
+        </button>
+      </div>
+      <div className="mt-20 text-lightCyan">
+        <span>Challenge by </span>
+        <span className="text-neonGreen">Frontend Mentor.</span>
+        <span>Coded by </span>
+        <span className="text-neonGreen">AOMSK.</span>
+      </div>
+    </main>
   );
 }
 
